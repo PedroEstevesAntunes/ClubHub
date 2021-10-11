13c2c8ec2e406f0c0ab1507dc038535e74e4d8b1
PedroEstevesAntunes <52006814+PedroEstevesAntunes@users.noreply.github.com>
2021-10-11 16:35:21 +0100

    NewDif

--- V1/ClubHub.js
+++ V1/ClubHub.js
@@ -1,1 +1,16 @@
-const nome = "Hospital"; // declara uma constante e atribui o valor "Hospital" (um 'string', uma cadeia de carateres)
+const nome = "ClubHub"; // declara uma constante e atribui o valor "ClubHub" (um 'string', uma cadeia de carateres)
+
+class Socio {
+    constructor(nome, sobrenome, email, ) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        /* test*/
+            
+    }
+
+    toString() {
+        return `${this.nome} (bloco ${this.bloco})`;
+    }
+    static contar() { return this.#quantidade }; 
+}
