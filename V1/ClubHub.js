1b55c79eeedab079f8aa8468d343ff759508fcee
PedroEstevesAntunes <52006814+PedroEstevesAntunes@users.noreply.github.com>
2021-10-12 09:26:53 +0100

    12102021-0926

--- V1/ClubHub.js
+++ V1/ClubHub.js
@@ -16,21 +16,15 @@ class Partner {
         return `${this.nome} ${this.surname}`;
     }
     get idade() {
-        // To implement
+        return this.nascimento ? new Date().getYear() - this.nascimento.getYear() : undefined;
     }
-    set registrationDate (){
-        // To implement
+    set registrationDate(data){
+        if (data.indexOf("/") > 0) {
+            const [dia, mês, ano] = data.split("/"); 
+            this.#registrationDate = new Date(ano, mês, dia); 
+        } else
+            this.#registrationDate = new Date(data); 
     }
-}
-
-
-class PartnerPayment{
-    constructor(partner, paymentDay, ) {
-        this.partner = partner;
-        this.paymentDay = paymentDay;        
-    }
-
-
 }
 
 
@@ -41,8 +35,23 @@ class PartnerLevel{
     }
 
     toString() {
-        return `${this.levelName} -  ${this.nome} `;
+        return `${this.levelName} -> Price: ${this.levelPrice}€ `;
     }
-
-
 }
+
+
+class PartnerPayment{
+    constructor(partner, paymentPrice, paymentday) {
+        this.partner = partner; 
+        this.paymentPrice = paymentPrice;
+        this.paymentday = paymentday;   
+    }
+}
+
+PartnerPayment.prototype.imprimir = function () {
+    return `Payment of ${this.partner} -> ${this.paymentPrice}€ -  ${this.paymentday}`;
+}
+
+const basic = new PartnerLevel("Basic", 15);
+const advanced = new PartnerLevel("Advanced", 30);
+const premium = new PartnerLevel("Premium", 80);
